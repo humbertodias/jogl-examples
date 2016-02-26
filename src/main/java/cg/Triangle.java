208ff9cb5f1c79ade615e6adc36d2cec9a8c5ede
Humberto Dias <hldtux@gmail.com>
2016-02-26 14:50:33 -0300

    Formatting

--- src/main/java/cg/Triangle.java
+++ src/main/java/cg/Triangle.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cg;
 
 import com.jogamp.opengl.GL2;
@@ -13,55 +8,60 @@ import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
 import javax.swing.JFrame;
 
-public class Triangle implements GLEventListener{
-   @Override
-   public void display(GLAutoDrawable drawable) {
-      final GL2 gl = drawable.getGL().getGL2();
-      gl.glBegin (GL2.GL_LINES);
-      //drawing the base
-      gl.glBegin (GL2.GL_LINES);
-      gl.glVertex3f(-0.50f, -0.50f, 0);
-      gl.glVertex3f(0.50f, -0.50f, 0);
-      gl.glEnd();
-      //drawing the right edge
-      gl.glBegin (GL2.GL_LINES);
-      gl.glVertex3f(0f, 0.50f, 0);
-      gl.glVertex3f(-0.50f, -0.50f, 0);
-      gl.glEnd();
-      //drawing the lft edge
-      gl.glBegin (GL2.GL_LINES);
-      gl.glVertex3f(0f, 0.50f, 0);
-      gl.glVertex3f(0.50f, -0.50f, 0);
-      gl.glEnd();
-      gl.glFlush();
-   }
-   @Override
-   public void dispose(GLAutoDrawable arg0) {
-      //method body
-   }
-   @Override
-   public void init(GLAutoDrawable arg0) {
-   // method body
-   }
-   @Override
-   public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
-   int arg4) {
-   // method body
-   }
-   public static void main(String[] args) {
-      //getting the capabilities object of GL2 profile
-      final GLProfile profile = GLProfile.get(GLProfile.GL2);
-      GLCapabilities capabilities = new GLCapabilities(profile);
-      // The canvas 
-      final GLCanvas glcanvas = new GLCanvas(capabilities);
-      Triangle l = new Triangle();
-      glcanvas.addGLEventListener(l);
-      glcanvas.setSize(400, 400);
-      //creating frame
-      final JFrame frame = new JFrame ("Triangle");
-      //adding canvas to frame
-      frame.getContentPane().add(glcanvas);
-      frame.setSize(frame.getContentPane().getPreferredSize());
-      frame.setVisible(true);
-   }//end of main
+public class Triangle implements GLEventListener {
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+        final GL2 gl = drawable.getGL().getGL2();
+        gl.glBegin(GL2.GL_LINES);
+        //drawing the base
+        gl.glBegin(GL2.GL_LINES);
+        gl.glVertex3f(-0.50f, -0.50f, 0);
+        gl.glVertex3f(0.50f, -0.50f, 0);
+        gl.glEnd();
+        //drawing the right edge
+        gl.glBegin(GL2.GL_LINES);
+        gl.glVertex3f(0f, 0.50f, 0);
+        gl.glVertex3f(-0.50f, -0.50f, 0);
+        gl.glEnd();
+        //drawing the lft edge
+        gl.glBegin(GL2.GL_LINES);
+        gl.glVertex3f(0f, 0.50f, 0);
+        gl.glVertex3f(0.50f, -0.50f, 0);
+        gl.glEnd();
+        gl.glFlush();
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable arg0) {
+        //method body
+    }
+
+    @Override
+    public void init(GLAutoDrawable arg0) {
+        // method body
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
+            int arg4) {
+        // method body
+    }
+
+    public static void main(String[] args) {
+        //getting the capabilities object of GL2 profile
+        final GLProfile profile = GLProfile.get(GLProfile.GL2);
+        GLCapabilities capabilities = new GLCapabilities(profile);
+        // The canvas 
+        final GLCanvas glcanvas = new GLCanvas(capabilities);
+        Triangle l = new Triangle();
+        glcanvas.addGLEventListener(l);
+        glcanvas.setSize(400, 400);
+        //creating frame
+        final JFrame frame = new JFrame("Triangle");
+        //adding canvas to frame
+        frame.getContentPane().add(glcanvas);
+        frame.setSize(frame.getContentPane().getPreferredSize());
+        frame.setVisible(true);
+    }//end of main
 }//end of classimport com.jogamp.opengl.GL2;
